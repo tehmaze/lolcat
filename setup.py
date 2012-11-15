af341434f09befde08356e9b7d315c6c1eaa6a1a
Wijnand Modderman-Lenstra <maze@pyth0n.org>
2012-11-15 09:40:14 +0100

    Added setup script

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='lolcat',
+      version='0.42',
+      description='Rainbows and unicorns!',
+      author='Wijnand Modderman-Lenstra',
+      author_email='maze@pyth0n.org',
+      url='https://github.com/tehmaze/lolcat/',
+      scripts=['lolcat'],
+     )
