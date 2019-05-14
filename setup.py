7be07d18b6be085e9021d8fa25773e53c425d5ed
maze <maze@booking.com>
2019-05-14 09:19:41 +0200

    setup.py: use README.md for description

--- setup.py
+++ setup.py
@@ -3,39 +3,49 @@
 from setuptools import setup
 
 
-LONG_DESCRIPTION = '''# LOLcat
+LONG_DESCRIPTION = '''
+LOLcat
+======
+
 Straight Python port of the Ruby version at https://github.com/busyloop/lolcat/
-but with 100% more fun, because, hey, it's not Ruby. There are no external
-dependencies..
+but with 100% more fun, because, hey, it's not Ruby. There are no external dependencies..
 
 It's a tool like cat, with added lulz.
 
-## Installing
+
+Installing
+==========
+
 To install the stable version:
 
-```
-$ pip install lolcat
-```
+    $ pip install lolcat
+
 
 To install the development version:
 
-```
-$ pip install git+https://github.com/tehmaze/lolcat.git
-```
+    $ pip install git+https://github.com/tehmaze/lolcat.git
 
-## Usage and examples
+
+Usage and examples
+==================
+
 ![](https://github.com/tehmaze/lolcat/raw/master/lolcat.png)
 
-## Demo
+
+Demo
+====
+
 Check out the ASCII cast at https://asciinema.org/a/1563
 
-## Bugs
+
+Bugs
+====
+
 Send a PR.
 '''
 
-
 setup(name='lolcat',
-      version='1.2',
+      version='1.3',
       description='Rainbows and unicorns!',
       long_description=LONG_DESCRIPTION,
       long_description_content_type='text/markdown',
